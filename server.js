6972e41cd4894ee45ea7cc5891533fe7e68dbb70
matienzo01 <matiasgutierrez225@gmail.com>
2021-12-12 18:57:49 -0300

    PseudoLogin implementado

--- server.js
+++ server.js
@@ -18,14 +18,19 @@ const server = http.createServer(function (req, res) {
 		let parsedUrl = new url.URL(req.url, "http://localhost:8080");
 		let search = parsedUrl.searchParams;
 		console.log(parsedUrl);
-		if (parsedUrl.search === "") {
+		if (parsedUrl.pathname === "/") {
 			inicializa().then((respuesta) => {
-				res.write(respuesta);
-				res.end();
+				res.end(respuesta);
 			});
 		} else if (parsedUrl.pathname.slice(0, 8) === "/filter") {
 			filtrar(search).then((respuesta) => {
 				res.end(respuesta);
+			});
+		} else if (parsedUrl.pathname.slice(0, 6) === "/login") {
+			iniciaSesion(JSON.parse(body)).then((respuesta) => {
+				if (respuesta === null) res.statusCode = 406;
+				else res.statusCode = 200;
+				res.end();
 			});
 		}
 	});
@@ -58,4 +63,20 @@ async function filtrar(params) {
 	console.log(documentos);
 	await client.close();
 	return JSON.stringify(documentos);
+}
+
+async function iniciaSesion(body) {
+	let usuario = body.identificador;
+	await client.connect();
+	const db = client.db(myDb);
+	const collection = db.collection("usuarios");
+	let document = await collection.findOne({ usuario: usuario });
+	console.log(document);
+	console.log(usuario);
+	if (document === null) {
+		return null;
+	} else {
+		console.log(document.contrasenia);
+		if (document.contrasenia !== body.contrasenia) return null;
+	}
 }
