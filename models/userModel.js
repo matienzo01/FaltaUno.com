931550c1404d6c1a45f4a91b014f24463cd97c34
matienzo01 <matiasgutierrez225@gmail.com>
2021-12-18 14:04:25 -0300

    cookie de nuevo funcionando

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv").config();
 
 const userSchema = new mongoose.Schema({
